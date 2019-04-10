a9a3e02ce8ecf838184dc100c89f26cda6c219e8
usseif <usseif97@gmail.com>
2019-04-10 13:02:28 +0200

    commit

---
+++ src/eg/edu/alexu/csd/filestructure/sort/cs19_cs61/Heap.java
@@ -0,0 +1,38 @@
+package eg.edu.alexu.csd.filestructure.sort.cs19_cs61;
+
+import java.util.Collection;
+
+import eg.edu.alexu.csd.filestructure.sort.IHeap;
+import eg.edu.alexu.csd.filestructure.sort.INode;
+
+public class Heap implements IHeap {
+	@Override
+	public INode getRoot() {
+		return null;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public void heapify(INode node) {
+
+	}
+
+	@Override
+	public Comparable extract() {
+		return null;
+	}
+
+	@Override
+	public void insert(Comparable element) {
+
+	}
+
+	@Override
+	public void build(Collection unordered) {
+
+	}
+}
---
+++ src/eg/edu/alexu/csd/filestructure/sort/cs19_cs61/Sort.java
@@ -0,0 +1,23 @@
+package eg.edu.alexu.csd.filestructure.sort.cs19_cs61;
+
+import java.util.ArrayList;
+
+import eg.edu.alexu.csd.filestructure.sort.IHeap;
+import eg.edu.alexu.csd.filestructure.sort.ISort;
+
+public class Sort implements ISort {
+	@Override
+	public IHeap heapSort(ArrayList unordered) {
+		return null;
+	}
+
+	@Override
+	public void sortSlow(ArrayList unordered) {
+
+	}
+
+	@Override
+	public void sortFast(ArrayList unordered) {
+
+	}
+}
